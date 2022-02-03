3f679b849827eb21ff7b65d74a300c100659630e
Mary Margaret <mmlawton15@gmail.com>
2022-02-03 08:51:29 -0500

    initial code setup

---
+++ index.js
@@ -0,0 +1,5 @@
+var dateToRemember = '2022-4-15';
+
+function timeUntil(endTime) {
+    const totalTime = Date.parse(endTime) - Date.parse(new Date()); //use the current date minus the anticipated date in preparation for counting the time between
+}
