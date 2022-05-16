9fc596be71fea5022b794701997131f5e39cdd44
Mary Margaret <mmlawton15@gmail.com>
2022-05-16 10:47:08 -0400

    working on pseudocoding plan of action moving forward

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//center box div
+//style div/add days, hours, minutes seconds
+//add a prompt to ask for date, time and title of event upon button click
+//console log to make sure it's recording, then set to a variable value
+//use local storage or another method to retain this date and time
+//make this populate on the screen
+
 const startingMinutes = 10;
 let time = startingMinutes * 60;
 
