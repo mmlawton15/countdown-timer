ccfa40e5e2abcef817d99174f12d33d541ee0b59
Mary Margaret <mmlawton15@gmail.com>
2022-05-16 10:09:14 -0400

    working on countdown timer functionality

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var dateToRemember = '2022-4-15';
+var dateToRemember = '2022-10-15';
 
 function timeUntil(endTime) {
-    const totalTime = Date.parse(endTime) - Date.parse(new Date()); //use the current date minus the anticipated date in preparation for counting the time between
-    const seconds = Math.floor((total/1000) % 60);
 }
