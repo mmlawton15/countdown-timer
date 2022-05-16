f5a8489e98199f140efefa176f960c1154d5f9a7
Mary Margaret <mmlawton15@gmail.com>
2022-05-16 10:44:16 -0400

    working on styling the box to the center of the screen before continuing on logic

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var dateToRemember = '2022-10-15';
+const startingMinutes = 10;
+let time = startingMinutes * 60;
 
-function timeUntil(endTime) {
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
 }
-
