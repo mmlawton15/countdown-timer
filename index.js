a1296c1de5a9733c9b21968b7e492a4734f0154f
Mary Margaret <mmlawton15@gmail.com>
2022-05-18 09:51:25 -0400

    styled modal and created funcitonality for closing/opening modal

--- index.js
+++ index.js
@@ -4,13 +4,17 @@
 //use local storage or another method to retain this date and time
 //make this populate on the screen
 
+//TIME VARIABLES
 const startingMinutes = 10;
 let time = startingMinutes * 60;
-
 const countdownEl = document.getElementById('countdown');
-
 setInterval(updateCountdown, 1000);
+//BUTTON VARIABLES
+var modal = document.getElementById("getStartedModal");
+var button = document.getElementById("startButton");
+var span = document.getElementsByClassName("close")[0];
 
+//Function for time countdown
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
@@ -19,4 +23,25 @@ function updateCountdown() {
 
     countdownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
+}
+
+//function for popping up modal
+button.onclick = function() {
+    modal.style.display = "block";
+}
+
+//close on click
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//close if window is selected
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function enterDateAndTime() {
+
 }
